2ff99a37862820aa684a7e212599584762e433e8
Chris Qualls <quallsc@comcast.net>
2016-09-15 20:52:28 -0700

    Added ruby code

---
+++ ruby/hamsters.rb
@@ -0,0 +1,17 @@
+puts "What is the hamster's name?"
+name = gets.chomp
+puts "Loud squeaks 1-10?"
+volume = gets.chomp.to_i
+puts "What color fur?"
+fur_color = gets.chomp
+puts "Good candidate for adoption? True/False"
+candidate = gets.chomp
+puts "Age"
+age = nil
+
+puts name
+puts volume
+puts fur_color
+puts candidate
+puts age
+
---
+++ ruby/secret_agents.rb
@@ -0,0 +1,7 @@
+def encrypt(input)
+	index = 0
+while index < input.length
+  input[index] = input[index].upcase!
+  index += 1
+end
+end
