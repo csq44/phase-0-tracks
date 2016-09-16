e9071041279f4596a70b625ebcdf0a6aa0df0fce
Chris Qualls <quallsc@comcast.net>
2016-09-15 21:30:06 -0700

    Added ruby code

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,17 +1,15 @@
-puts "What is the hamster's name?"
+puts "What is your hamster's name?"
 name = gets.chomp
-puts "Loud squeaks 1-10?"
+puts "How loud would you like your hamster 1-10?"
 volume = gets.chomp.to_i
-puts "What color fur?"
-fur_color = gets.chomp
-puts "Good candidate for adoption? True/False"
+puts "What fur color?"
+color = gets.chomp
+puts "Is the hamster a good candidate for adoption?"
 candidate = gets.chomp
-puts "Age"
-age = nil
-
-puts name
-puts volume
-puts fur_color
-puts candidate
-puts age
-
+puts "How old is the hamster?"
+age = gets.chomp.to_i
+puts "The hamsters name is #{name}"
+puts "The hamster's volume is #{volume}"
+puts "The color is #{color}"
+puts "The hamster is a good candidate? #{candidate}"
+puts "The hamster is #{age} years old"
